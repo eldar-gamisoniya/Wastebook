d7b72942d6bce0611ddadc55f3c1c6e58e781265
Eldar Gamisoniya <eldar.gamisoniya@gmail.com>
2017-08-06 01:52:43 +0300

    Add test async component

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const App = () => (
-  <div>
-    Oh, hi, mark
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      asyncComponent: null,
+    };
+  }
+
+  async componentDidMount() {
+    import('./AsyncComponent').then(this.handleComponentLoad);
+  }
+
+  handleComponentLoad = (module) => {
+    this.setState(() => ({
+      asyncComponent: module.default,
+    }));
+  }
+
+  render() {
+    const AsyncComponent = this.state.asyncComponent;
+
+    return (
+      <div>
+        Hi, everybody
+        { AsyncComponent ? <AsyncComponent /> : (<div>Loading...</div>) }
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ client/AsyncComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AsyncComponent = () => (
+  <div>Some async component</div>
+);
+
+export default AsyncComponent;
