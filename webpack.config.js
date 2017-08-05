056dd889b873dc072bad61331f1dc21baff203be
Eldar Gamisoniya <eldar.gamisoniya@gmail.com>
2017-08-05 16:33:41 +0300

    Babel loader add cache

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,9 @@ module.exports = {
         exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+          },
         },
       },
       {
