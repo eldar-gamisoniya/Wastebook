4a9dca59040de05a090ebee583f7cb7184e4bb72
Eldar Gamisoniya <eldar.gamisoniya@gmail.com>
2017-08-08 00:52:08 +0300

    Restored exclude in webpack

--- client/AsyncComponent.jsx
+++ client/AsyncComponent.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './AsyncComponent.css';
 
 const AsyncComponent = () => (
   <div className={styles.someStyle}>Some async component</div>
 );
+
+AsyncComponent.propTypes = {
+  optArray: PropTypes.string.isRequired,
+};
 
 export default AsyncComponent;
--- scripts/webpack.client.prod.js
+++ scripts/webpack.client.prod.js
@@ -41,6 +41,7 @@ module.exports = {
       },
       {
         test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
           options: {
