49704f6dc6978e6c8079fcba7b53389c90ba9402
Eldar Gamisoniya <eldar.gamisoniya@gmail.com>
2017-08-24 19:21:59 +0300

    add airbnb config

--- client/shared/AsyncComponent/index.js
+++ client/shared/AsyncComponent/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './style.css';
 
 const AsyncComponent = ({ number }) =>
   <div className={styles.someStyle}>
     Some async component {number}
   </div>;
+
+AsyncComponent.propTypes = {
+  number: PropTypes.number.isRequired,
+};
+
 export default AsyncComponent;
