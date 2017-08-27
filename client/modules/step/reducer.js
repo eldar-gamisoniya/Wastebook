0907f96924cb873e4c38093412adb3ae6cb88aa5
Eldar Gamisoniya <eldar.gamisoniya@gmail.com>
2017-08-27 05:23:34 +0300

    modify step reducer to pass correct steps

--- client/modules/step/reducer.js
+++ client/modules/step/reducer.js
@@ -2,22 +2,46 @@ import * as constants from './constants';
 
 const initialState = {};
 
-export default (state = initialState, { type, payload }) => {
-  const sequenceName = payload && payload.sequence;
-  const sequence = state[sequenceName] || 0;
+const sequenceInitialState = { currentStep: 0, errors: {} };
+const sequenceReducer = (state = sequenceInitialState, { type, payload }) => {
+  let nextStep;
   switch (type) {
     case constants.NEXT_STEP:
-      return { ...state, [sequenceName]: sequence + 1 };
+      nextStep = state.currentStep + 1;
+      while (state.errors[nextStep] === false) nextStep += 1;
+      return {
+        currentStep: nextStep,
+        errors: { ...state.errors, [state.currentStep]: false },
+      };
     case constants.PREVIOUS_STEP:
-      return { ...state, [sequenceName]: Math.max(sequence - 1, 0) };
-    case constants.STEP_FAILURE:
       return {
         ...state,
-        [sequenceName]: payload.beginFromStart ? 0 : Math.max(payload.step, 0),
+        currentStep: Math.max(state.currentStep - 1, 0),
+      };
+    case constants.STEP_FAILURE:
+      return {
+        currentStep: payload.beginFromStart ? 0 : Math.max(payload.step, 0),
+        errors: { ...state.errors, [payload.step]: true },
       };
     default:
       return state;
   }
 };
 
-export const getCurrentStep = (state, sequence) => state[sequence] || 0;
+export default (state = initialState, action) => {
+  const sequenceName = action.payload && action.payload.sequence;
+  const sequence = state[sequenceName];
+  switch (action.type) {
+    case constants.NEXT_STEP:
+      return { ...state, [sequenceName]: sequenceReducer(sequence, action) };
+    case constants.PREVIOUS_STEP:
+      return { ...state, [sequenceName]: sequenceReducer(sequence, action) };
+    case constants.STEP_FAILURE:
+      return { ...state, [sequenceName]: sequenceReducer(sequence, action) };
+    default:
+      return state;
+  }
+};
+
+export const getCurrentStep = (state, sequence) =>
+  (state[sequence] && state[sequence].currentStep) || 0;
