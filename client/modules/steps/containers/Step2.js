e560f0c3b102e34910c2fe8d46db882afd68dfae
Eldar Gamisoniya <eldar.gamisoniya@gmail.com>
2017-08-27 03:36:56 +0300

    Add withStep to step2

--- client/modules/steps/containers/Step2.js
+++ client/modules/steps/containers/Step2.js
@@ -1,19 +1,60 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Field } from 'redux-form';
+import { compose, withHandlers } from 'recompose';
 
 import withStep from 'modules/step';
-
 import Step from './Step';
+import { callIfChanged } from '../utils';
 
-export const StepComponent = () =>
+const isValid = value => Boolean(value);
+
+const validate = value => (!isValid(value) ? 'Should be set' : undefined);
+
+const StepComponent = ({ onChange }) =>
   <Step>
     <label>
-      <Field name="b" component="input" type="radio" value="B1" /> B1
+      <Field
+        name="b"
+        component="input"
+        type="radio"
+        validate={validate}
+        value="B1"
+        onChange={onChange}
+      />{' '}
+      B1
     </label>
     <label>
-      <Field name="b" component="input" type="radio" value="B2" />
+      <Field
+        name="b"
+        component="input"
+        type="radio"
+        validate={validate}
+        value="B2"
+        onChange={onChange}
+      />
       B2
     </label>
   </Step>;
 
-export default withStep('challenge', 1, { showIfPassed: true })(StepComponent);
+StepComponent.propTypes = {
+  onChange: PropTypes.func.isRequired,
+};
+
+export default compose(
+  withStep('challenge', 1, { showIfPassed: true }),
+  withHandlers({
+    onChange: ({ onStepPassed, onStepFailed }) => (
+      event,
+      newValue,
+      previousValue,
+    ) =>
+      callIfChanged(
+        isValid,
+        newValue,
+        previousValue,
+        onStepPassed,
+        onStepFailed,
+      ),
+  }),
+)(StepComponent);
