71f5bb427d357377507245f0e628b76797da9a66
Eldar Gamisoniya <eldar.gamisoniya@gmail.com>
2017-09-05 01:55:24 +0300

    check if onLoad function called only once per request and client load

--- client/modules/challenge/indexSync.js
+++ client/modules/challenge/indexSync.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { reducer as formReducer } from 'redux-form';
 
 import { reducer as stepReducer, STEP_REDUCER_KEY } from 'modules/step';
@@ -6,9 +7,12 @@ import Challenge from './containers/Challenge';
 import saga from './sagas';
 
 export const initModule = store => {
+  console.log('\n\ninit called\n\n');
   injectReducer(store, 'form', formReducer);
   injectReducer(store, STEP_REDUCER_KEY, stepReducer);
   injectSaga(store, saga);
 };
 
 export default Challenge;
+
+export const hello = () => <div>Hello, World</div>;
